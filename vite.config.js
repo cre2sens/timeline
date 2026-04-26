4456072cb095b697d7292434770882ba08356d20
cre2sens <cre2sens@gmail.com>
2026-04-26 23:57:01 +0900

    Add China events and fix github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,31 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 const cesiumSource = 'node_modules/cesium/Build/Cesium'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: `${cesiumSource}/Workers`, dest: 'cesium' },
-        { src: `${cesiumSource}/ThirdParty`, dest: 'cesium' },
-        { src: `${cesiumSource}/Assets`, dest: 'cesium' },
-        { src: `${cesiumSource}/Widgets`, dest: 'cesium' },
-      ],
-    }),
-  ],
-  define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium'),
-  },
-  optimizeDeps: {
-    include: ['cesium', 'mersenne-twister'],
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-  },
+export default defineConfig(({ command }) => {
+  const isProd = command === 'build';
+  const base = isProd ? '/timeline/' : '/';
+
+  return {
+    base,
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          { src: `${cesiumSource}/Workers`, dest: 'cesium' },
+          { src: `${cesiumSource}/ThirdParty`, dest: 'cesium' },
+          { src: `${cesiumSource}/Assets`, dest: 'cesium' },
+          { src: `${cesiumSource}/Widgets`, dest: 'cesium' },
+        ],
+      }),
+    ],
+    define: {
+      CESIUM_BASE_URL: JSON.stringify(isProd ? '/timeline/cesium' : '/cesium'),
+    },
+    optimizeDeps: {
+      include: ['cesium', 'mersenne-twister'],
+    },
+    build: {
+      chunkSizeWarningLimit: 5000,
+    },
+  };
 })
